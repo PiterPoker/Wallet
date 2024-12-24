fc23cb65229e47df588ab950d23ff49f0ade4f9f
PiterPoker <tema5286@gmail.com>
2024-12-25 00:35:51 +0300

    feat: Added dbContext

---
+++ Wallet.Infrastructure/WalletDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wallet.Domain.Models.AccountOfPerson;
+using Wallet.Infrastructure.Configurations;
+using Models = Wallet.Domain.Models.WalletOfFamily;
+
+namespace Wallet.Infrastructure;
+
+public class WalletDBContext : DbContext
+{
+    public DbSet<Models.Family> Families { get; set; }
+    public DbSet<Models.Owner> Owners { get; set; }
+    public DbSet<Models.Wallet> Wallets { get; set; }
+    public DbSet<Models.SubWallet> SubWallets { get; set; }
+    public DbSet<Account> Accounts { get; set; }
+    public WalletDBContext(DbContextOptions<WalletDBContext> options) : base(options)
+    { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new FamilyConfiguration());
+        modelBuilder.ApplyConfiguration(new OwnerConfiguration());
+        modelBuilder.ApplyConfiguration(new WalletConfiguration());
+        modelBuilder.ApplyConfiguration(new SubWalletConfiguration());
+        modelBuilder.ApplyConfiguration(new AccountConfiguration());
+    }
+}
