6ba4cccdb8eec998cd801869066da56408453ab6
PiterPoker <tema5286@gmail.com>
2025-01-28 22:56:58 +0300

    feat: Implemented models mapping

---
+++ Wallet.API/Mapping/FamilyMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Wallet.API.Models.WalletOfFamily;
+using Wallet.Domain.Models.WalletOfFamily;
+
+namespace Wallet.API.Mapping;
+
+public class FamilyMapping : Profile
+{
+    public FamilyMapping()
+    {
+        CreateMap<Family, FamilyReadModel>()
+            .MaxDepth(3);
+        CreateMap<FamilyWriteModel, Family>();
+        CreateMap<HeadMember, HeadMemberReadModel>()
+            .MaxDepth(3);
+        CreateMap<HeadMemberWriteModel, HeadMember>();
+    }
+}
